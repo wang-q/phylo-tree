539f7aac36cf905c02ced9b77b09dec809565230
wang-q <wang-q@outlook.com>
2016-01-02 07:16:00 +0800

    Working example

--- tree.pl
+++ tree.pl
@@ -5,5 +5,30 @@ use autodie;
 
 use FindBin;
 use Path::Tiny;
+use YAML qw(Dump Load DumpFile LoadFile);
 
-my $template = path($FindBin::RealBin, "template.tex");
+my $forest;
+
+if (defined $ARGV[0] and path($ARGV[0])->is_file) {
+    $forest = path($ARGV[0])->slurp;
+    if (defined $ARGV[1] and path($ARGV[1])->is_file) {
+        my @trans = path($ARGV[1])->lines({chomp => 1});
+        for my $tran (@trans) {
+            my ($from, $to) = split /,/, $tran;
+            next unless defined $from and defined $to;
+            $forest =~ s/\b$from\b/$to/g;
+        }
+    }
+}
+
+my $template = path($FindBin::RealBin, "template.tex")->slurp;
+
+if (defined $forest) {
+    $template =~ s/\%BEGIN.+\%END/$forest/s;
+}
+
+path("out.tex")->spew($template);
+
+__END__
+
+perl tree.pl forest/test.forest forest/translation.csv
