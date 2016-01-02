13e9afc98567d8f016e5913538c204da75c28b30
wang-q <wang-q@outlook.com>
2016-01-02 16:08:20 +0800

    Change output paths

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 Create phylogenetics trees by xelatex/tikz/forest.
 
+The following command
+
 ```bash
 perl tree.pl forest/test.forest forest/translation.csv
-xelatex out.tex
 ```
+
+will create `forest/test.trans.tex` and `forest/test.trans.pdf`.
--- tree.pl
+++ tree.pl
@@ -8,10 +8,15 @@ use Path::Tiny;
 use YAML qw(Dump Load DumpFile LoadFile);
 
 my $forest;
+my $outdir = ".";
+my $outbase = "template";
 
 if (defined $ARGV[0] and path($ARGV[0])->is_file) {
     $forest = path($ARGV[0])->slurp;
+    $outdir = path($ARGV[0])->parent->absolute->stringify;
+    $outbase = path($ARGV[0])->basename(".forest", ".tree");
     if (defined $ARGV[1] and path($ARGV[1])->is_file) {
+        $outbase .= ".trans";
         my @trans = path($ARGV[1])->lines({chomp => 1});
         for my $tran (@trans) {
             my ($from, $to) = split /,/, $tran;
@@ -27,6 +32,11 @@ if (defined $forest) {
     $template =~ s/\%BEGIN.+\%END/$forest/s;
 }
 
-path("out.tex")->spew($template);
+my $outfile = path($outdir, $outbase . ".tex" )->stringify;
+path($outfile)->spew($template);
+
+chdir $outdir;
+system "latexmk -xelatex $outfile";
+system "latexmk -c $outfile";
 
 __END__
