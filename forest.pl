b0dc3c38b738ad323bdb554abdd6f2d8e707c3eb
wang-q <wang-q@outlook.com>
2016-01-02 22:10:45 +0800

    Rename tree.pl to forest.pl
    Add CLI options

---
+++ forest.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use autodie;
+
+use Getopt::Long qw(HelpMessage);
+use FindBin;
+use YAML qw(Dump Load DumpFile LoadFile);
+
+use Path::Tiny;
+
+#----------------------------------------------------------#
+# GetOpt section
+#----------------------------------------------------------#
+
+GetOptions(
+    'help|?'    => sub { HelpMessage(0) },
+    'trans|t=s' => \( my $translation ),
+    'reverse|r' => \( my $reverse ),
+    'pdf|p'     => \( my $create_pdf ),
+) or HelpMessage(1);
+
+my $forest;
+my $outdir  = ".";
+my $outbase = "template";
+
+if ( defined $ARGV[0] and path( $ARGV[0] )->is_file ) {
+    $forest  = path( $ARGV[0] )->slurp;
+    $outdir  = path( $ARGV[0] )->parent->absolute->stringify;
+    $outbase = path( $ARGV[0] )->basename( ".forest", ".tree" );
+}
+
+if ( defined $translation and path($translation)->is_file ) {
+    $outbase .= ".trans";
+    my @trans = path($translation)->lines( { chomp => 1 } );
+    for my $tran (@trans) {
+        my ( $from, $to ) = split /,/, $tran;
+        next unless defined $from and defined $to;
+        $forest =~ s/\b$from\b/$to/gi;
+    }
+}
+
+my $template = path( $FindBin::RealBin, "template.tex" )->slurp;
+
+if ( defined $forest ) {
+    $template =~ s/\%BEGIN.+\%END/$forest/s;
+}
+
+if ($reverse) {
+    if ( $template =~ s/\s+^.*tree_direction.*$//m ) {
+        warn "Draw reverse tree\n";
+    }
+}
+
+my $outfile = path( $outdir, $outbase . ".tex" )->stringify;
+path($outfile)->spew($template);
+
+if ($create_pdf) {
+    chdir $outdir;
+    system "latexmk -xelatex $outfile";
+    system "latexmk -c $outfile";
+}
+
+__END__
