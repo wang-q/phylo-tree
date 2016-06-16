48ebf56ddb57c0b50aa8fea1b0824307fb833d52
wang-q <wang-q@outlook.com>
2016-06-17 06:08:05 +0800

    Can read from stdin

--- forest.pl
+++ forest.pl
@@ -3,9 +3,9 @@ use strict;
 use warnings;
 use autodie;
 
-use Getopt::Long qw(HelpMessage);
+use Getopt::Long;
 use FindBin;
-use YAML qw(Dump Load DumpFile LoadFile);
+use YAML::Syck;
 
 use Path::Tiny;
 
@@ -19,7 +19,8 @@ forest.pl - tikz/forest to tex
 
 =head1 SYNOPSIS
 
-    perl forest.pl <forest file> [options]
+    perl forest.pl <infile> [options]
+        <infile> is a forest file, stdin for STDIN.
       Options:
         --help          -?          brief help message
         --trans         -t  STR     translation file, csv
@@ -30,53 +31,58 @@ forest.pl - tikz/forest to tex
 =cut
 
 GetOptions(
-    'help|?'    => sub { HelpMessage(0) },
+    'help|?'    => sub { Getopt::Long::HelpMessage(0) },
     'trans|t=s' => \( my $translation ),
     'append|a'  => \( my $append ),
     'reverse|r' => \( my $reverse ),
     'pdf|p'     => \( my $create_pdf ),
-) or HelpMessage(1);
+) or Getopt::Long::HelpMessage(1);
 
 my $forest;
 my $outdir  = ".";
-my $outbase = "template";
+my $outbase = "output";
 
-if ( defined $ARGV[0] and path( $ARGV[0] )->is_file ) {
-    $forest  = path( $ARGV[0] )->slurp;
-    $outdir  = path( $ARGV[0] )->parent->absolute->stringify;
-    $outbase = path( $ARGV[0] )->basename( ".forest", ".tree" );
+if ( defined $ARGV[0] ) {
+    if ( lc $ARGV[0] eq "stdin" ) {
+        $forest = do { local $/; <STDIN> };
+    }
+    elsif ( path( $ARGV[0] )->is_file ) {
+        $forest  = path( $ARGV[0] )->slurp;
+        $outdir  = path( $ARGV[0] )->parent->absolute->stringify;
+        $outbase = path( $ARGV[0] )->basename( ".forest", ".tree" );
+    }
 }
+
+# print YAML::Syck::Dump($forest);
 
 #----------------------------------------------------------#
 # Run
 #----------------------------------------------------------#
 
-if ( defined $translation and path($translation)->is_file ) {
-    $outbase .= ".trans";
-    my @lines = path($translation)->lines( { chomp => 1 } );
-    for my $line (@lines) {
-        next if $line =~ /^#/;
-        my ( $from, $to ) = split /,/, $line;
-        next unless defined $from and defined $to;
-        if ($append) {
-            $forest =~ s/\b$from\b/$from $to/gi;
-        }
-        else {
-            $forest =~ s/\b$from\b/$to/gi;
-        }
-    }
-}
-
 my $template = path( $FindBin::RealBin, "template.tex" )->slurp;
 
-if ( defined $forest ) {
+if ( defined $forest and $forest =~ /\[/ ) {
+    if ( defined $translation and path($translation)->is_file ) {
+        $outbase .= ".trans";
+        my @lines = path($translation)->lines( { chomp => 1 } );
+        for my $line (@lines) {
+            next if $line =~ /^#/;
+            my ( $from, $to ) = split /,/, $line;
+            next unless defined $from and defined $to;
+            if ($append) {
+                $forest =~ s/\b$from\b/$from $to/gi;
+            }
+            else {
+                $forest =~ s/\b$from\b/$to/gi;
+            }
+        }
+    }
+
     $template =~ s/\%BEGIN.+\%END/$forest/s;
 }
 
 if ($reverse) {
-    if ( $template =~ s/\s+^.*tree_direction.*$//m ) {
-        warn "Draw reverse tree\n";
-    }
+    $template =~ s/\s+^.*tree_direction.*$//m;
 }
 
 my $outfile = path( $outdir, $outbase . ".tex" )->stringify;
